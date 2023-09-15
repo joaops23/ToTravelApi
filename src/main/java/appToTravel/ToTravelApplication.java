4090e1be7e6ba75d127646701b7f63f20081ccc4
= <=>
2023-09-15 20:04:21 -0300

    :TADA: Ćommit inicial

---
+++ src/main/java/appToTravel/ToTravelApplication.java
@@ -0,0 +1,13 @@
+package appToTravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToTravelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/appToTravel/ToTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package appToTravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToTravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
